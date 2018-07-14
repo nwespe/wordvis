0a229963803828641f2f480271da3f1d54887f88
nwespe <nicholewespe@gmail.com>
2018-07-13 23:02:21 -0400

    First attempt to modify for taxonomy.

--- wordvis.py
+++ wordvis.py
@@ -5,13 +5,18 @@ import sys
 import codecs
 
 '''
-Word Visualiser
+Taxonomy Visualizer
 ---------------
 
-This utility reads a text file containing word frequency data, and generates a Sunburst Chart illustrating the frequency
-with which letters appear at each position within the words.
+Modified from Word Visualiser, Copyright 2016 Rob Dawson
 
-Each line of the input file must contain a single word, followed by a tab character, followed by a numeric value.
+    https://github.com/codebox/wordvis
+    http://codebox.org.uk/pages/common-english-words-visualisation
+
+This utility reads a text file containing taxonomy information and frequency data, and generates a Sunburst Chart illustrating the frequency
+with which each taxonomic classification is present.
+
+Each line of the input file must contain a semicolon-delimited taxonomy, followed by a tab character, followed by a numeric value.
 For example:
 
 THE	53097401461
@@ -21,17 +26,14 @@ TO	19347398077
 
 A correctly formatted file containing about 100,000 words can be found at:
 
-    http://norvig.com/google-books-common-words.txt
+    <example file here>
 
-To run the utility supply the path to the word file, and the path to the svg file as follows:
+To run the utility supply the path to the taxonomy file, and the path to the svg file as follows:
 
-    python wordvis.py words.txt word_chart.svg
+    python sunburst.py taxonomies.txt taxonomy_chart.svg
 
 
-Copyright 2016 Rob Dawson
 
-    https://github.com/codebox/wordvis
-    http://codebox.org.uk/pages/common-english-words-visualisation
 '''
 
 START = '^'
@@ -49,15 +51,15 @@ ENCODING = 'utf-8'
 
 
 class Node:
-    def __init__(self, letter):
-        self.letter = letter
+    def __init__(self, clade):
+        self.clade = clade
         self.count = 0
         self.children = {}
 
 
 class EndNode:
     def __init__(self):
-        self.letter = END
+        self.clade = END
         self.count = 1
         self.children = {}
 
@@ -65,23 +67,23 @@ class EndNode:
 class Tree:
     def __init__(self):
         self.root = Node(START)
-        self.letters = set()
+        self.clades = set()
 
-    def add(self, word, count):
-        def add_chars(parent_node, chars, count):
+    def add(self, taxonomy, count):
+        def add_clades(parent_node, clades, count):
             parent_node.count += count
-            if len(chars) == 0:
+            if len(clades) == 0:
                 parent_node.children[END] = EndNode()
                 return
 
-            first_letter = chars[0]
-            if first_letter not in parent_node.children:
-                parent_node.children[first_letter] = Node(first_letter)
+            first_clade = clades[0]
+            if first_clade not in parent_node.children:
+                parent_node.children[first_clade] = Node(first_clade)
 
-            add_chars(parent_node.children[first_letter], chars[1:], count)
+            add_clades(parent_node.children[first_clade], clades[1:], count)
 
-        self.letters.update(list(word))
-        add_chars(self.root, word, count)
+        self.clades.update(list(taxonomy))
+        add_clades(self.root, taxonomy, count)  # make taxonomy string a list
 
 
 class Svg:
@@ -103,7 +105,7 @@ class Svg:
     def add_path(self, d, clazz):
         self.content.append(u'<path d="{0}" class="{1}"/>'.format(d, clazz))
 
-    def add_segment(self, letter, start_x1, start_y1, end_x1, end_y1, start_x2, start_y2, end_x2, end_y2, r1, r2):
+    def add_segment(self, clade, start_x1, start_y1, end_x1, end_y1, start_x2, start_y2, end_x2, end_y2, r1, r2):
         path = u'M{0} {1} A {2} {3}, 0, 0, 1, {4} {5} L {6} {7} A {8} {9}, 0, 0, 0, {10} {11} Z'.format(
             start_x1, start_y1,
             r1, r1,
@@ -112,7 +114,7 @@ class Svg:
             r2, r2,
             start_x2, start_y2
         )
-        self.add_path(path, letter)
+        self.add_path(path, clade)
 
     def save(self, out_file):
         part1, tmp = self.template.split('%style%')
@@ -130,29 +132,29 @@ class Svg:
 
 
 class CircleDiagram:
-    def __init__(self, svg, letters):
+    def __init__(self, svg, clades):
         self.ring_count = 0
         self.svg = svg
         self.center = (size/2, size/2)
-        self.last_letter_pos = (0, 0)
-        self.letters = letters
-        self.letter_colours = dict(zip(self.letters, map(self._colour_for_letter, self.letters)))
+        self.last_clade_pos = (0, 0)
+        self.clades = clades
+        self.clade_colours = dict(zip(self.clades, map(self._colour_for_clade, self.clades)))
 
-        for letter in letters:
-            svg.add_styles('.' + letter, {'fill': self._colour_for_letter(letter), 'stroke': LINE_COLOUR})
+        for clade in clades:
+            svg.add_styles('.' + clade, {'fill': self._colour_for_clade(clade), 'stroke': LINE_COLOUR})
 
         svg.add_styles('text', {'fill': FONT_COLOUR, 'font-family': FONT_NAME, 'font-size': FONT_SIZE})
 
-    def _colour_for_letter(self, letter):
-        letter_index = self.letters.index(letter)
-        letter_count = len(self.letters)
-        rgb = colorsys.hls_to_rgb(letter_index / letter_count, COLOUR_LIGHTNESS, 1)
+    def _colour_for_clade(self, clade):
+        clade_index = self.clades.index(clade)
+        clade_count = len(self.clades)
+        rgb = colorsys.hls_to_rgb(clade_index / clade_count, COLOUR_LIGHTNESS, 1)
         return '#' + ''.join('%02x' % i for i in map(lambda x: x * 255, rgb))
 
     def _calc_coords(self, r, a):
         return self.center[0] + math.sin(a) * r, self.center[1] + -math.cos(a) * r
 
-    def _draw_segment(self, letter, level, start_angle, end_angle):
+    def _draw_segment(self, clade, level, start_angle, end_angle):
         r1 = RING_DEPTH * level
         r2 = RING_DEPTH * (level + 1)
 
@@ -161,23 +163,23 @@ class CircleDiagram:
         end_x1, end_y1 = self._calc_coords(r1, end_angle)
         end_x2, end_y2 = self._calc_coords(r2, end_angle)
 
-        self.svg.add_segment(letter, start_x1, start_y1, end_x1, end_y1, start_x2, start_y2, end_x2, end_y2, r1, r2)
+        self.svg.add_segment(clade, start_x1, start_y1, end_x1, end_y1, start_x2, start_y2, end_x2, end_y2, r1, r2)
 
-    def _draw_letters(self, level, all_letters):
+    def _draw_clades(self, level, all_clades):
         # Draw as many letters as we can without overlapping them.
 
         # Calculate cartesian co-ordinates for each letter
-        letter_coords = []
+        clade_coords = []
         radius = RING_DEPTH * (level + 0.5)
-        for letter, start_angle, end_angle in all_letters:
+        for clade, start_angle, end_angle in all_clades:
             x, y = self._calc_coords(radius, (start_angle + end_angle) / 2)
             x -= 4 * SCALE
             y += 4 * SCALE
 
-            letter_coords.append((letter, end_angle - start_angle, x, y))
+            clade_coords.append((clade, end_angle - start_angle, x, y))
 
         # Sort letter coordinates by size of the associated segment (largest first)
-        letter_coords.sort(key=lambda n: n[1], reverse=True)
+        clade_coords.sort(key=lambda n: n[1], reverse=True)
         occupied = []
 
         def is_room(x, y):
@@ -188,27 +190,27 @@ class CircleDiagram:
             return True
 
         # O(n^2) performance blackspot here, but still runs fine for largest inputs so leaving
-        for letter, _, x, y in letter_coords:
+        for clade, _, x, y in clade_coords:
             if is_room(x, y):
-                self.svg.add_text(letter.upper(), x, y)
+                self.svg.add_text(clade.upper(), x, y)
                 occupied.append((x, y))
 
     def add_ring(self, parts):
         level = self.ring_count
 
         # Draw all the segments first
-        all_letters = []
+        all_clades = []
         for p in parts:
             letter = p[0]
             start_angle = p[2] * math.pi * 2
             end_angle = (p[2] + p[1]) * math.pi * 2
 
-            all_letters.append((letter, start_angle, end_angle))
+            all_clades.append((clade, start_angle, end_angle))
 
-            self._draw_segment(letter, level+1, start_angle, end_angle)
+            self._draw_segment(clade, level+1, start_angle, end_angle)
 
         # Draw letters on top of segments so we can read them
-        self._draw_letters(level+1, all_letters)
+        self._draw_clades(level+1, all_clades)
 
         self.ring_count += 1
 
@@ -247,20 +249,20 @@ class Rings:
 
 args = sys.argv
 if len(args) != 3:
-    print "Usage: python {0} <word file> <svg file>".format(args[0])
+    print "Usage: python {0} <taxonomy file> <svg file>".format(args[0])
     sys.exit(1)
 
-word_file = args[1]
+tax_file = args[1]
 svg_file = args[2]
 
 tree = Tree()
-for line in codecs.open(word_file, encoding=ENCODING).readlines():
-    word, count = line.split('\t')
-    tree.add(word.lower().strip(), int(count))
+for line in codecs.open(tax_file, encoding=ENCODING).readlines():
+    taxonomy, count = line.split('\t')
+    tree.add(taxonomy.split(';'), int(count))
 
 size = MAX_RINGS * RING_DEPTH * 2
 svg = Svg(size, size)
-diagram = CircleDiagram(svg, sorted(tree.letters))
+diagram = CircleDiagram(svg, sorted(tree.clades))
 
 rings = Rings(tree)
 for ring in rings.get():
